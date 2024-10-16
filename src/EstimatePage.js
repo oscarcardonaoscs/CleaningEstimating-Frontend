fc2565149cddcb125a5a7de0ceb6aaaa727e29c8
Oscar Cardona <oscar.cardona.oscs@gmail.com>
2024-10-16 18:54:07 -0400

    Logo y fondo en Resumen de estimado

--- src/EstimatePage.js
+++ src/EstimatePage.js
@@ -1,4 +1,5 @@
 import { useNavigate, useLocation } from "react-router-dom";
+import logo from "./Logo_MCJ.png";
 
 const EstimatePage = () => {
   const location = useLocation();
@@ -25,49 +26,63 @@ const EstimatePage = () => {
     });
 
   return (
-    <div className="max-w-md mx-auto bg-white p-6 rounded-md shadow-md">
-      <h2 className="text-2xl font-bold mb-4">Estimate Summary</h2>
-      <div className="mb-4">
-        <p>
-          <strong>House Size (sq ft):</strong> {formData.size}
-        </p>
+    <div className="max-w-md mx-auto p-4">
+      {/* Logo */}
+      <div className="flex justify-between items-center mb-4">
+        <img src={logo} alt="Logo" className="w-32 h-auto" />
       </div>
-      <div className="mb-4">
-        <p>
-          <strong>Cleaning Type:</strong> {formData.cleaningType}
-        </p>
-        {/* Mostrar la frecuencia seleccionada debajo del tipo de limpieza */}
-        {formData.frequency && (
-          <p>
-            <strong>Frequency:</strong> {formData.frequency}
-          </p>
-        )}
-        <p>
-          <strong>Estimate:</strong> ${estimate.toFixed(2)}
-        </p>
-      </div>
-      {/* Show additional services only if applicable */}
-      {formData.cleaningType === "Regular" &&
-        additionalServicesList.length > 0 && (
-          <div className="mb-4">
-            <h3 className="font-semibold mb-2">Additional Services:</h3>
-            <ul className="list-disc list-inside">
-              {additionalServicesList.map((service, index) => (
-                <li key={index}>{service}</li>
-              ))}
-            </ul>
-          </div>
-        )}
 
-      {/* Button to re-calculate */}
-      <button
-        onClick={() =>
-          navigate("/", { state: { formData, additionalServices } })
-        }
-        className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
-      >
-        Re-calculate
-      </button>
+      <div className="p-5 max-w-lg mx-auto bg-white shadow-md rounded-lg">
+        <h2 className="text-2xl font-bold mb-4" style={{ color: "#164e63" }}>
+          Estimate Summary
+        </h2>
+        <div className="mb-4">
+          <p>
+            <strong>House Size:</strong> {formData.size} sqft
+          </p>
+        </div>
+        <div className="mb-4">
+          <p>
+            <strong>Cleaning Type:</strong> {formData.cleaningType}
+          </p>
+          {/* Mostrar la frecuencia seleccionada debajo del tipo de limpieza */}
+          {formData.frequency && (
+            <p>
+              <strong>Frequency:</strong> {formData.frequency}
+            </p>
+          )}
+          <p>
+            <strong>Estimate:</strong> ${estimate.toFixed(2)}
+          </p>
+        </div>
+        {/* Show additional services only if applicable */}
+        {formData.cleaningType === "Regular" &&
+          additionalServicesList.length > 0 && (
+            <div className="mb-4">
+              <h3 className="font-semibold mb-2">Additional Services:</h3>
+              <ul className="list-disc list-inside">
+                {additionalServicesList.map((service, index) => (
+                  <li key={index}>{service}</li>
+                ))}
+              </ul>
+            </div>
+          )}
+
+        <div className="flex justify-between mt-4">
+          {/* Button to re-calculate */}
+          <button
+            onClick={() =>
+              navigate("/", { state: { formData, additionalServices } })
+            }
+            className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
+          >
+            Re-calculate
+          </button>
+          <button className="bg-green-500 text-white py-2 px-4 rounded-md hover:bg-green-600">
+            BOOK NOW
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
