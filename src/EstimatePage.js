5bf333b8ae646f90e451ed8bd004df0b35011f67
Oscar Cardona <oscar.cardona.oscs@gmail.com>
2025-03-03 20:53:24 -0600

    update redirect Re-calculate from / to /estimating

--- src/EstimatePage.js
+++ src/EstimatePage.js
@@ -190,7 +190,9 @@ const EstimatePage = () => {
         <div className="flex justify-between mt-6 ">
           <button
             onClick={() =>
-              navigate("/", { state: { formData, additionalServices } })
+              navigate("/estimating", {
+                state: { formData, additionalServices },
+              })
             }
             className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600"
           >
